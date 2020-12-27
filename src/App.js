781a2d7ed128b1c5f9daa33d959e1262faa5a6e2
Jack <jack.newman12@gmail.com>
2020-12-27 11:58:03 +0000

    Add title bar and button color junk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
 import Button from '@material-ui/core/Button'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Fab from '@material-ui/core/Fab';
+import RefreshIcon from '@material-ui/icons/Refresh';
 import { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import { Toolbar } from '@material-ui/core';
+import './App.css';
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -20,58 +24,62 @@ const rows = [
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-function App() {
+function PowerButton(props) {
   let [isDisabled, setisDisabled] = useState(false);
+  let [isPowered, setisPowered] = useState(props.PowerState);
 
   let Clicked = () => {
     setisDisabled(true);
-    setTimeout(() => {setisDisabled(false)}, 2000);  
+    console.log(`Toggling ${props.DevName}`);
+    setTimeout(() => { setisDisabled(false); setisPowered(!isPowered)}, 2000);
   };
+
+  return <Button variant="contained" style={{backgroundColor:isPowered? "green":"red"}} disabled={isDisabled} onClick={Clicked} >
+    {isDisabled ? "Loading" : (isPowered ? "On" : "Off")}</Button>
+}
+
+function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          
-      <Table className={"JackTest"} aria-label="simple table" style={{"background-color": "white"}}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-              <TableCell><Button variant="contained" color="default" disabled={isDisabled} onClick={Clicked} >
-            {isDisabled ? "Loading...": "Toggle"}
-          </Button></TableCell>
+      <AppBar >
+        <Toolbar >
+          <img src={logo} className="App-logo" alt="logo" height={40} />
+          Power Supply Thingo
+          <Fab color="secondary" aria-label="add" style={{ margin: 0, right: 20, position: 'fixed' }}>
+            <RefreshIcon />
+          </Fab>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <div>
+        <Table className={"JackTest"} aria-label="simple table" style={{backgroundColor: "white" }}>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{fontWeight:900}}>Dessert (100g serving)</TableCell>
+              <TableCell style={{fontWeight:900}}>Calories</TableCell>
+              <TableCell style={{fontWeight:900}}>Fat&nbsp;(g)</TableCell>
+              <TableCell style={{fontWeight:900}}>Carbs&nbsp;(g)</TableCell>
+              <TableCell style={{fontWeight:900}}>Protein&nbsp;(g)</TableCell>
+              <TableCell style={{fontWeight:900}}>Toggle Power</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell>{row.calories}</TableCell>
+                <TableCell>{row.fat}</TableCell>
+                <TableCell>{row.carbs}</TableCell>
+                <TableCell>{row.protein}</TableCell>
+                <TableCell><PowerButton DevName={row.name} PowerState={false}></PowerButton></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
