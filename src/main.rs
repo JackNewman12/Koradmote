d6001538d55190aa3eef8482ceb746b805046e54
Jack <jack.newman12@gmail.com>
2020-12-28 08:52:25 +0000

    Rust mutex stuff

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,60 @@
 extern crate rocket;
 extern crate rocket_contrib;
 
+use std::sync::Mutex;
+use std::collections::HashMap;
+
 use serde::Serialize;
 
+use rocket::State;
 use rocket_contrib::json::Json;
 use rocket_contrib::serve::StaticFiles;
 
-#[derive(Serialize, Clone)]
-struct Device {
-    name: String,
-    state: bool,
+#[derive(Serialize, Clone, Default)]
+struct DeviceState {
+    // voltage: u32,
+    // current: u32,
+    power: bool,
 }
 
+#[derive(Serialize, Clone, Default)]
+struct Device {
+    connection: String, // TODO will be some sort of Serial device type
+    state: DeviceState,
+}
+
+type DeviceList = Mutex<HashMap<String, Device>>;
+
 #[get("/")]
-fn devices() -> Json<Vec<Device>> {
-    Json([Device{name:"One".to_string(), state:true}, 
-          Device{name:"Two".to_string(), state:false}].to_vec())
+fn devices(devs: State<DeviceList>) -> Json<Vec<DeviceState>> {
+    let data = devs.lock().unwrap();
+    Json(data.clone().into_iter().map(|(k, d)| d.state).collect())
 }
 
 #[get("/<name>")]
-fn device(name: String) -> Json<Device> {
-    Json(Device{name:name, state:true})
+fn device(name: String, devs: State<DeviceList>) -> Json<Device> {
+    let data = devs.lock().unwrap();
+    Json(data.get(&name).unwrap().clone())
+    // Json(Device{name:name, state:true})
 }
 
 #[get("/<name>/toggle/<state>")]
 fn toggledevice(name: String, state: bool) -> Json<Device> {
-    Json(Device{name:name, state:state})
+    // Json(Device{name:name, state:state})
+    Json(Default::default())
 }
 
 fn main() {
-    rocket::ignite()
+    let rocket = rocket::ignite()
     .mount("/device", routes![devices, device, toggledevice])
     .mount("/", StaticFiles::from("build/"))
-    .launch();
+    .manage(DeviceList::new(HashMap::new()));
+
+    let current_devices = rocket.state::<DeviceList>().unwrap();
+    {
+    let mut devlist = current_devices.lock().unwrap();
+    devlist.insert("One".to_string(), Device{..Default::default()});
+    }
+
+    rocket.launch();
 }
