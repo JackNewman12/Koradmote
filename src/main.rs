6274b823160cc9d287570666a77a0ae99360eb22
Jack <jack.newman12@gmail.com>
2020-12-28 03:51:45 +0000

    Proper rust output examples

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,31 @@
 extern crate rocket;
 extern crate rocket_contrib;
 
+use serde::Serialize;
+
+use rocket_contrib::json::Json;
 use rocket_contrib::serve::StaticFiles;
 
+#[derive(Serialize, Clone)]
+struct Device {
+    name: String,
+    state: bool,
+}
+
 #[get("/")]
-fn devices() -> &'static str {
-    "[{'wow':3}, {'swag':4}]"
+fn devices() -> Json<Vec<Device>> {
+    Json([Device{name:"One".to_string(), state:true}, 
+          Device{name:"Two".to_string(), state:false}].to_vec())
 }
 
 #[get("/<name>")]
-fn device(name: String) -> String {
-    format!("{{{}:3}}", name)
+fn device(name: String) -> Json<Device> {
+    Json(Device{name:name, state:true})
 }
 
 #[get("/<name>/toggle/<state>")]
-fn toggledevice(name: String, state: bool) -> String {
-    format!("{{{}:{}}}", name, state)
+fn toggledevice(name: String, state: bool) -> Json<Device> {
+    Json(Device{name:name, state:state})
 }
 
 fn main() {
