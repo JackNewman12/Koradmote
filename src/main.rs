3cf2787232e5bad0d001024aac8e86256dc5516e
Jack <jack.newman12@gmail.com>
2020-12-28 03:27:38 +0000

    add example device output

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
+extern crate rocket_contrib;
 
-use std::io;
-use std::path::{Path, PathBuf};
-
-use std::env;
-
-use rocket::response::NamedFile;
+use rocket_contrib::serve::StaticFiles;
 
 #[get("/")]
-fn index() -> io::Result<NamedFile> {
-    println!("\nCurrent path is {:?}", env::current_dir().unwrap());
-    // NamedFile::open("build/static/index.html")
-    NamedFile::open("build/index.html")
+fn devices() -> &'static str {
+    "[{'wow':3}, {'swag':4}]"
 }
 
-#[get("/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("build/").join(file)).ok()
+#[get("/<name>")]
+fn device(name: String) -> String {
+    format!("{{{}:3}}", name)
+}
+
+#[get("/<name>/toggle/<state>")]
+fn toggledevice(name: String, state: bool) -> String {
+    format!("{{{}:{}}}", name, state)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, files]).launch();
+    rocket::ignite()
+    .mount("/device", routes![devices, device, toggledevice])
+    .mount("/", StaticFiles::from("build/"))
+    .launch();
 }
